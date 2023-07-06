7ca613cf496076268963eb61d2438acc07d931fa
Saiyed Irmi <irsa@odoo.com>
2023-07-06 18:55:57 +0530

    department

--- models/department.py
+++ models/department.py
@@ -6,6 +6,9 @@ class HospitalDepartment(models.Model):
 
 	name=fields.Char(string="Department name",required=True)
 	description=fields.Char(string="Department details")
+	appointments=fields.One2many('hospital.appointment','doctor_id',string='appointments')
+
+	
 	
 
 
