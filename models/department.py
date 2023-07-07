beb4f42b60b5b405a608c14db662939842ab1eaa
Saiyed Irmi <irsa@odoo.com>
2023-07-07 18:33:02 +0530

    changes in department

--- models/department.py
+++ models/department.py
@@ -6,7 +6,8 @@ class HospitalDepartment(models.Model):
 
 	name=fields.Char(string="Department name",required=True)
 	description=fields.Char(string="Department details")
-	appointments=fields.One2many('hospital.appointment','doctor_id',string='appointments')
+	appointments=fields.One2many('hospital.appointment','appointment_id',string='appointments')
+	doctor_id=fields.Many2one('hospital.doctor',string='doctor')
 
 	
 	
