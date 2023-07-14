a22a251ef17b5c3d73068bb15450d23514af9a76
Saiyed Irmi <irsa@odoo.com>
2023-07-14 18:30:41 +0530

    some changes

--- models/doctor.py
+++ models/doctor.py
@@ -10,6 +10,12 @@ class HospitalDoctor(models.Model):
    specialization=fields.Char('specialization')
    appointments=fields.One2many('hospital.appointment','doctor_id',string='appointments')
    appointment_count_dector=fields.Integer(string='appointment count',compute="_compute_appointment_count_dector")
+   state=fields.Selection([
+      ('draft','Draft'),
+      ('in_consultation','In_Consultation'),
+      ('done','Done'),
+      ('cancelled','Cancelled'),
+   ],string='state',required=True,default='draft')
 
    def _compute_appointment_count_dector(self):
       self.appointment_count_dector = self.env['hospital.appointment'].search_count([('doctor_id', '=', self.id)])
